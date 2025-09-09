7d5d0c2c1cd9efbad8cf21bde8323f430febbce0
Prajapati Hiten Mahendrabhai <156202976+Prajapati-HitenCode@users.noreply.github.com>
2025-09-09 10:56:51 +0530

    Add files via upload

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,18 +1,12 @@
-// package com.example;
-
-// /**
-//  * Hello world!
-//  */
-// public class App {
-//     public static void main(String[] args) {
-//         System.out.println("Hello World!");
-//     }
-// }
 package com.example;
 
+/**
+ * Hello world!
+ */
 public class App {
-    public String greet() {
-        return "Hello World!";
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
+
 
